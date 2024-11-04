4c7d00c07c7f7fe59512276ecd556eb727e692dc
Kamlesh mani Bhairam <papabhairamk205@gmail.com>
2024-11-04 18:50:51 +0530

    Day 35,01 commiting day 28,03

---
+++ Day 28/leetcode_problem_700.java
@@ -0,0 +1,35 @@
+// 700. Search in a Binary Search Tree.
+
+public class leetcode_problem_700{
+    public static void main(String [] args){
+
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while(root != null && root.val != val){
+            if(root.val < val){
+                root = root.right;
+            }else{
+                root = root.left;
+            }
+        } 
+        return root;      
+    }
+}
